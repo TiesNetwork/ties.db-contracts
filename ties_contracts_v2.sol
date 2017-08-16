f54c7189e84aaef93287af46e1f948c89951a11f
Anton Filatov <ya-enot@mail.ru>
2017-08-16 18:10:16 +0300

    Refactored to split to libraries

--- ties_contracts_v2.sol
+++ ties_contracts_v2.sol
@@ -29,12 +29,12 @@ library TiesLibAddress {
 }
 
 interface TiesDBRestrictions {
-    function canCreateTablespace(bytes32 tsKey, address owner) public constant returns (bool);
-    function canDeleteTablespace(bytes32 tsKey, address owner) public constant returns (bool);
-    function canCreateTable(bytes32 tsKey, bytes32 tKey, address owner) public constant returns (bool);
-    function canDeleteTable(bytes32 tsKey, bytes32 tKey, address owner) public constant returns (bool);
-    function canCreateField(bytes32 tsKey, bytes32 tKey, bytes32 fKey, address owner) public constant returns (bool);
-    function canDeleteField(bytes32 tsKey, bytes32 tKey, bytes32 fKey, address owner) public constant returns (bool);
+    function canCreateTablespace(string tsName, address owner) public constant returns (bool);
+    function canDeleteTablespace(string tsName, address owner) public constant returns (bool);
+    function canCreateTable(string tsName, string tName, address owner) public constant returns (bool);
+    function canDeleteTable(string tsName, string tName, address owner) public constant returns (bool);
+    function canCreateField(string tsName, string tName, string fName, address owner) public constant returns (bool);
+    function canDeleteField(string tsName, string tName, string fName, address owner) public constant returns (bool);
 }
 
 contract TiesDBRestrictionsOwner {
@@ -44,249 +44,274 @@ contract TiesDBRestrictionsOwner {
 
     mapping(bytes32 => address) private tsm;
 
-    function registerOwner(bytes32 tsKey, address owner) public {
-        require(tsKey != 0);
-        require(tsm[tsKey].isFree());
-        tsm[tsKey] = owner;
+    function registerOwner(string tsName, address owner) public {
+        require(!tsName.isEmpty());
+        require(tsm[tsName.hash()].isFree());
+        tsm[tsName.hash()] = owner;
     }
 
-    function canCreateTablespace(bytes32 tsKey, address owner) public constant returns (bool) {
-        require(tsKey != 0);
-        return tsm[tsKey] == owner;
+    function canCreateTablespace(string tsName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
 
-    function canDeleteTablespace(bytes32 tsKey, address owner) public constant returns (bool) {
-        require(tsKey != 0);
-        return tsm[tsKey] == owner;
+    function canDeleteTablespace(string tsName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
 
-    function canCreateTable(bytes32 tsKey, bytes32 tKey, address owner) public constant returns (bool) {
-        require(tsKey != 0 && tKey != 0);
-        return tsm[tsKey] == owner;
+    function canCreateTable(string tsName, string tName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty() && !tName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
 
-    function canDeleteTable(bytes32 tsKey, bytes32 tKey, address owner) public constant returns (bool) {
-        require(tsKey != 0 && tKey != 0);
-        return tsm[tsKey] == owner;
+    function canDeleteTable(string tsName, string tName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty() && !tName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
 
-    function canCreateField(bytes32 tsKey, bytes32 tKey, bytes32 fKey, address owner) public constant returns (bool) {
-        require(tsKey != 0 && tKey != 0 && fKey != 0);
-        return tsm[tsKey] == owner;
+    function canCreateField(string tsName, string tName, string fName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty() && !tName.isEmpty() && !fName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
-    function canDeleteField(bytes32 tsKey, bytes32 tKey, bytes32 fKey, address owner) public constant returns (bool) {
-        require(tsKey != 0 && tKey != 0 && fKey != 0);
-        return tsm[tsKey] == owner;
+    function canDeleteField(string tsName, string tName, string fName, address owner) public constant returns (bool) {
+        require(!tsName.isEmpty() && !tName.isEmpty() && !fName.isEmpty());
+        return tsm[tsName.hash()] == owner;
     }
 }
 
-contract TiesDBCore {
-    
+library TiesDBLibField {
+
     using TiesLibAddress for address;
-    using TiesLibString for string;
-
-    mapping(bytes32 => Tablespace) internal tsm;
-    bytes32[] tsmis;
-    
-    struct Tablespace {
-        TiesDBRestrictions rs;
-        string name;
-        uint256 tsmi;
-        mapping(bytes32 => Table) tm;
-        bytes32[] tmis;
-    }
-
-    struct Table {
-        Tablespace ts;
-        string name;
-        uint256 tmi;
-        mapping(bytes32 => Field) fm;
-        bytes32[] fmis;
-    }
+    using TiesDBLibField for TiesDBLibField.Field;
 
     struct Field {
-        Table t;
+        TiesDBLibTable.Table t;
         string name;
         uint256 fmi;
     }
 
-    function _isSet(Tablespace ts) constant internal returns (bool) {
-        return !address(ts.rs).isFree();
-    }
-
-    function _isSet(Table t) constant internal returns (bool) {
-        return !address(t.ts.rs).isFree();
-    }
-
-    function _isSet(Field f) constant internal returns (bool) {
+    function isSet(Field storage f) constant internal returns (bool) {
         return !address(f.t.ts.rs).isFree();
     }
 
-    function getTablespaceKeys() constant public returns (bytes32[]) {
-        return tsmis;
-    }
-
-    function _deleteTable(Tablespace storage ts, bytes32 tKey) internal {
-        if(ts.tmis.length > 1){
-            ts.tmis[ts.tm[tKey].tmi] = ts.tmis[ts.tmis.length-1];
-        }
-        ts.tmis.length--;
-        delete ts.tm[tKey];
-    }
-
-    function _deleteField(Table storage t, bytes32 fKey) internal {
-        if(t.fmis.length > 1){
-            t.fmis[t.fm[fKey].fmi] = t.fmis[t.fmis.length-1];
-        }
-        t.fmis.length--;
-        delete t.fm[fKey];
-    }
-
-    function getVersion() constant public returns (string);
-}
-
-contract TiesDBTablespace is TiesDBCore {
-
-    function createTablespace(string tsName, TiesDBRestrictions rs) public returns (bytes32) {
-        var tsKey = tsName.hash();
-        require(!_isSet(tsm[tsKey]) && rs.canCreateTablespace(tsKey, msg.sender));
-        tsm[tsName.hash()] = Tablespace({name: tsName, rs: rs, tsmi: tsmis.length, tmis: new bytes32[](0)});
-        tsmis.push(tsKey);
-        return tsKey;
-    }
-
-    function deleteTablespace(bytes32 tsKey) public {
-        var ts = tsm[tsKey];
-        require(_isSet(ts) && ts.rs.canDeleteTablespace(tsKey, msg.sender));
-        if(tsmis.length > 1){
-            tsmis[ts.tsmi] = tsmis[tsmis.length-1];
-        }
-        tsmis.length--;
-    }
-
-    function hasTablespace(bytes32 tsKey) constant public returns (bool) {
-        return _isSet(tsm[tsKey]);
-    }
-    
-    function getTablespaceName(bytes32 tsKey) constant public returns (string) {
-        var ts = tsm[tsKey];
-        require(_isSet(ts));
-        return ts.name;
-    }
-
-    function getTablespaceTablesKeys(bytes32 tsKey) constant public returns (bytes32[]) {
-        var ts = tsm[tsKey];
-        require(_isSet(ts));
-        return ts.tmis;
+    function getFieldName(Field storage f) constant internal returns (string) {
+        require(f.isSet());
+        return f.name;
     }
 }
 
-contract TiesDBTable is TiesDBCore {
+library TiesDBLibTable {
 
     using TiesLibString for string;
+    using TiesLibAddress for address;
+    using TiesDBLibTable for TiesDBLibTable.Table;
+    using TiesDBLibField for TiesDBLibField.Field;
 
-    function createTable(bytes32 tsKey, string tName) public returns (bytes32) {
-        var tKey = tName.hash();
-        var ts = tsm[tsKey];
-        require(!hasTable(tsKey, tKey) && ts.rs.canCreateTable(tsKey, tKey, msg.sender));
-        ts.tm[tKey] = Table({name: tName, ts: ts, tmi: ts.tmis.length, fmis: new bytes32[](0)});
-        ts.tmis.push(tKey);
-        return tKey;
+    struct Table {
+        TiesDBLibTablespace.Tablespace ts;
+        string name;
+        uint256 tmi;
+        mapping(bytes32 => TiesDBLibField.Field) fm;
+        bytes32[] fmis;
     }
 
-    function deleteTable(bytes32 tsKey, bytes32 tKey) public {
-        var ts = tsm[tsKey];
-        var t = ts.tm[tKey];
-        require(_isSet(t) && ts.rs.canDeleteTable(tsKey, tKey, msg.sender));
-        if(ts.tmis.length > 1){
-            ts.tmis[t.tmi] = ts.tmis[ts.tmis.length-1];
-        }
-        ts.tmis.length--;
+    function isSet(Table storage t) constant internal returns (bool) {
+        return !address(t.ts.rs).isFree();
     }
 
-    function hasTable(bytes32 tsKey, bytes32 tKey) constant public returns (bool) {
-        return _isSet(tsm[tsKey].tm[tKey]);
-    }
-    
-    function getTableName(bytes32 tsKey, bytes32 tKey) constant public returns (string) {
-        var t = tsm[tsKey].tm[tKey];
-        require(_isSet(t));
+    function getTableName(Table storage t) constant internal returns (string) {
+        require(t.isSet());
         return t.name;
     }
 
-    function getTableFieldsKeys(bytes32 tsKey, bytes32 tKey) constant public returns (bytes32[]) {
-        var t = tsm[tsKey].tm[tKey];
-        require(_isSet(t));
+    function getTableFieldsKeys(Table storage t) constant internal returns (bytes32[]) {
+        require(t.isSet());
         return t.fmis;
     }
-}
 
-contract TiesDBField is TiesDBCore {
-
-    using TiesLibString for string;
-
-    function createField(bytes32 tsKey, bytes32 tKey, string fName) public returns (bytes32) {
+    function createField(Table storage t, string fName) internal returns (bytes32) {
         var fKey = fName.hash();
-        var t = tsm[tsKey].tm[tKey];
-        require(_isSet(t) && !_isSet(t.fm[fKey]) && t.ts.rs.canCreateField(tsKey, tKey, fKey, msg.sender));
-        t.fm[fKey] = Field({name: fName, t: t, fmi: t.fmis.length});
+        require(!t.hasField(fKey) && t.ts.rs.canCreateField(t.ts.name, t.name, fName, msg.sender));
+        t.fm[fKey] = TiesDBLibField.Field({name: fName, t: t, fmi: t.fmis.length});
         t.fmis.push(fKey);
         return fKey;
     }
 
-    function deleteField(bytes32 tsKey, bytes32 tKey, bytes32 fKey) public returns (bytes32) {
-        var t = tsm[tsKey].tm[tKey];
+    function deleteField(Table storage t, bytes32 fKey) internal returns (bytes32) {
         var f = t.fm[fKey];
-        require(_isSet(f) && t.ts.rs.canDeleteField(tsKey, tKey, fKey, msg.sender));
+        require(f.isSet() && t.ts.rs.canDeleteField(t.ts.name, t.name, f.name, msg.sender));
         if(t.fmis.length > 1){
             t.fmis[f.fmi] = t.fmis[t.fmis.length-1];
         }
         t.fmis.length--;
     }
 
+    function hasField(Table storage t, bytes32 fKey) constant internal returns (bool) {
+        return t.fm[fKey].isSet();
+    }
+}
+
+library TiesDBLibTablespace {
+
+    using TiesLibString for string;
+    using TiesLibAddress for address;
+    using TiesDBLibTable for TiesDBLibTable.Table;
+    using TiesDBLibTablespace for TiesDBLibTablespace.Tablespace;
+
+    struct Tablespace {
+        TiesDBRestrictions rs;
+        string name;
+        uint256 tsmi;
+        mapping(bytes32 => TiesDBLibTable.Table) tm;
+        bytes32[] tmis;
+    }
+
+    function isSet(Tablespace storage ts) constant internal returns (bool) {
+        return !address(ts.rs).isFree();
+    }
+    
+    function getTablespaceName(Tablespace storage ts) constant internal returns (string) {
+        require(ts.isSet());
+        return ts.name;
+    }
+
+    function getTablespaceTablesKeys(Tablespace storage ts) constant internal returns (bytes32[]) {
+        require(ts.isSet());
+        return ts.tmis;
+    }
+
+    function createTable(Tablespace storage ts, string tName) internal returns (bytes32) {
+        var tKey = tName.hash();
+        require(!ts.hasTable(tKey) && ts.rs.canCreateTable(ts.name, tName, msg.sender));
+        ts.tm[tKey] = TiesDBLibTable.Table({name: tName, ts: ts, tmi: ts.tmis.length, fmis: new bytes32[](0)});
+        ts.tmis.push(tKey);
+        return tKey;
+    }
+
+    function deleteTable(Tablespace storage ts, bytes32 tKey) internal {
+        var t = ts.tm[tKey];
+        require(t.isSet() && ts.rs.canDeleteTable(ts.name, t.name, msg.sender));
+        if(ts.tmis.length > 1){
+            ts.tmis[t.tmi] = ts.tmis[ts.tmis.length-1];
+        }
+        ts.tmis.length--;
+    }
+
+    function hasTable(Tablespace storage ts, bytes32 tKey) constant internal returns (bool) {
+        return ts.tm[tKey].isSet();
+    }
+}
+
+library TiesDBLibStorage {
+
+    using TiesLibString for string;
+    using TiesLibAddress for address;
+    using TiesDBLibStorage for TiesDBLibStorage.Storage;
+    using TiesDBLibTablespace for TiesDBLibTablespace.Tablespace;
+
+    struct Storage {
+        mapping(bytes32 => TiesDBLibTablespace.Tablespace) tsm;
+        bytes32[] tsmis;
+    }
+
+    function createTablespace(Storage storage s, string tsName, TiesDBRestrictions rs) internal returns (bytes32) {
+        var tsKey = tsName.hash();
+        require(!s.hasTablespace(tsKey) && rs.canCreateTablespace(tsName, msg.sender));
+        s.tsm[tsName.hash()] = TiesDBLibTablespace.Tablespace({name: tsName, rs: rs, tsmi: s.tsmis.length, tmis: new bytes32[](0)});
+        s.tsmis.push(tsKey);
+        return tsKey;
+    }
+
+    function deleteTablespace(Storage storage s, bytes32 tsKey) internal {
+        var ts = s.tsm[tsKey];
+        require(ts.isSet() && ts.rs.canDeleteTablespace(ts.name, msg.sender));
+        if(s.tsmis.length > 1){
+            s.tsmis[ts.tsmi] = s.tsmis[s.tsmis.length-1];
+        }
+        s.tsmis.length--;
+    }
+
+    function hasTablespace(Storage storage s, bytes32 tsKey) constant internal returns (bool) {
+        return s.tsm[tsKey].isSet();
+    }
+
+    function getTablespaceKeys(Storage storage s) constant internal returns (bytes32[]) {
+        return s.tsmis;
+    }
+}
+
+contract TiesDB {
+    
+    using TiesDBLibStorage for TiesDBLibStorage.Storage;
+    using TiesDBLibTablespace for TiesDBLibTablespace.Tablespace;
+    using TiesDBLibTable for TiesDBLibTable.Table;
+    using TiesDBLibField for TiesDBLibField.Field;
+
+    TiesDBLibStorage.Storage private s;
+
+    function createTablespace(string tsName, TiesDBRestrictions rs) public returns (bytes32) {
+        return s.createTablespace(tsName, rs);
+    }
+
+    function deleteTablespace(bytes32 tsKey) public {
+        s.deleteTablespace(tsKey);
+    }
+
+    function getTablespaceKeys() constant public returns (bytes32[]) {
+        return s.getTablespaceKeys();
+    }
+
+    function hasTablespace(bytes32 tsKey) constant public returns (bool) {
+        return s.hasTablespace(tsKey);
+    }
+    
+    function getTablespaceName(bytes32 tsKey) constant public returns (string) {
+        return s.tsm[tsKey].getTablespaceName();
+    }
+
+    function getTablespaceTablesKeys(bytes32 tsKey) constant public returns (bytes32[]) {
+        return s.tsm[tsKey].getTablespaceTablesKeys();
+    }
+
+    function createTable(bytes32 tsKey, string tName) public returns (bytes32) {
+        return s.tsm[tsKey].createTable(tName);
+    }
+
+    function deleteTable(bytes32 tsKey, bytes32 tKey) public {
+        s.tsm[tsKey].deleteTable(tKey);
+    }
+
+    function hasTable(bytes32 tsKey, bytes32 tKey) constant public returns (bool) {
+        return s.tsm[tsKey].hasTable(tKey);
+    }
+
+    function getTableName(bytes32 tsKey, bytes32 tKey) constant public returns (string) {
+        return s.tsm[tsKey].tm[tKey].getTableName();
+    }
+
+    function getTableFieldsKeys(bytes32 tsKey, bytes32 tKey) constant public returns (bytes32[]) {
+        return s.tsm[tsKey].tm[tKey].getTableFieldsKeys();
+    }
+
+    function createField(bytes32 tsKey, bytes32 tKey, string fName) public returns (bytes32) {
+        return s.tsm[tsKey].tm[tKey].createField(fName);
+    }
+
+    function deleteField(bytes32 tsKey, bytes32 tKey, bytes32 fKey) public returns (bytes32) {
+        s.tsm[tsKey].tm[tKey].deleteField(fKey);
+    }
+
     function hasField(bytes32 tsKey, bytes32 tKey, bytes32 fKey) constant public returns (bool) {
-        return _isSet(tsm[tsKey].tm[tKey].fm[fKey]);
+        return s.tsm[tsKey].tm[tKey].hasField(fKey);
     }
 
     function getFieldName(bytes32 tsKey, bytes32 tKey, bytes32 fKey) constant public returns (string) {
-        var f = tsm[tsKey].tm[tKey].fm[fKey];
-        require(_isSet(f));
-        return f.name;
-    }
-
-}
-
-contract TiesDB is TiesDBTablespace, TiesDBTable, TiesDBField {
-    function getVersion() constant public returns (string) {
-        return "0.1.0 alpha";
+        return s.tsm[tsKey].tm[tKey].fm[fKey].getFieldName();
     }
 }
 
 contract Debug is TiesDB, TiesDBRestrictionsOwner {
-
-    using TiesLibString for string;
-
-    function registerOwner(string tsName) public {
-        registerOwner(tsName.hash(), msg.sender);
-    }
-    function canCreateTablespace(string tsName) public constant returns (bool) {
-        return canCreateTablespace(tsName.hash(), msg.sender);
-    }
-    function canDeleteTablespace(string tsName) public constant returns (bool) {
-        return canDeleteTablespace(tsName.hash(), msg.sender);
-    }
-    function canCreateTable(string tsName, string tName) public constant returns (bool) {
-        return canCreateTable(tsName.hash(), tName.hash(), msg.sender);
-    }
-    function canCreateTable(bytes32 tsKey, string tName) public constant returns (bool) {
-        return canCreateTable(tsKey, tName.hash(), msg.sender);
-    }
-    function canDeleteTable(string tsName, string tName) public constant returns (bool) {
-        return canDeleteTable(tsName.hash(), tName.hash(), msg.sender);
-    }
-    function canDeleteTable(bytes32 tsKey, string tName) public constant returns (bool) {
-        return canDeleteTable(tsKey, tName.hash(), msg.sender);
-    }
 
     function createTablespace(string tsName) public returns (bytes32) {
         return createTablespace(tsName, TiesDBRestrictions(this));
