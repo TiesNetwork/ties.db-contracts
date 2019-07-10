c6a41f8eed5db24722d03af6c612db617dcfca71
Anton Filatov <ya-enot@mail.ru>
2019-07-10 04:22:01 +0300

    Added default development network

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1337
+    },
     devlocal: {
       host: "localhost",
       port: 18545,
